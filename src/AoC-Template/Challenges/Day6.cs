e623e42fb52e09998b6446e4c0d3d8b87fb7ac0c
vosvosvos <indvfx@gmail.com>
2022-12-06 20:00:13 +0100

    Day 6 Yawn

---
+++ src/AoC-Template/Challenges/Day6.cs
@@ -0,0 +1,49 @@
+﻿using AoC.Running;
+using Iced.Intel;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Challenges
+{
+    public class Day6 : BaseChallenge
+    {
+        private readonly string Input;
+
+        public Day6()
+        {
+            Input = LoadInput(File.ReadAllText, nameof(Day6));
+        }
+
+        public override string SolvePartOne()
+        {
+            for (int i = 0; i < Input.Length; i++)
+            {
+                if (new HashSet<char>(Input.Substring(i, 4)).Count == 4)
+                {
+                    return (i + 4).ToString();
+                }
+            }
+            return "-1";
+        }
+
+        public override string SolvePartTwo()
+        {
+            for (int i = 0; i < Input.Length; i++)
+            {
+                if (new HashSet<char>(Input.Substring(i, 14)).Count == 14)
+                {
+                    return (i + 14).ToString();
+                }
+            }
+            return "-1";
+        }
+    }
+}
+
+
